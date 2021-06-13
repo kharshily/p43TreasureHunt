5811c05a85321b8c9071d3557da35ff72431d6f2
kharshily <70810464+kharshily@users.noreply.github.com>
2021-06-13 18:48:24 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white");
+    textSize(15);
+    text("R E V B A I L A", 100,50);
+    fill("lightblue");
+    text("Hint: Always changing, not constant !!", 100,70);
+// add code for displaying rest of the names and the hints.
+    
+    fill("white");
+    textSize(15);
+    text("A T D A S B A E", 700,150);
+    fill("lightblue");
+    text("Hint: Place where data is stored!", 700,170);
+
+    fill("white");
+    textSize(15);
+    text("C U T N I F O N", 400,300);
+    fill("lightblue");
+    text("Hint: To do a particular task!", 400,320);
+}
